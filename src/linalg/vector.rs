bf5836aa4b830f6d09f57e7244142106b90a4157
Daniel Lacina <daniellacina8@gmail.com>
2025-09-01 22:34:25 -0500

    added tests for vectors

--- src/linalg/vector.rs
+++ src/linalg/vector.rs
@@ -41,3 +41,51 @@ impl Hash for Vector {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_and_data() {
+        let data = vec![1.0, 2.5, 3.0];
+        let vector = Vector::new(data.clone());
+        assert_eq!(vector.data(), &data, "The data getter should return the original vector data.");
+    }
+
+    #[test]
+    fn test_squared_distance_correctness() {
+        let vec1 = Vector::new(vec![1.0, 2.0, 3.0]);
+        let vec2 = Vector::new(vec![4.0, 5.0, 6.0]);
+        
+        let expected_distance = 27.0;
+        
+        assert_eq!(vec1.squared_distance(&vec2), expected_distance);
+    }
+    
+    #[test]
+    fn test_squared_distance_to_self_is_zero() {
+        let vec1 = Vector::new(vec![10.5, -5.0, 0.0]);
+        let expected_distance = 0.0;
+
+        assert_eq!(vec1.squared_distance(&vec1), expected_distance, "The distance to itself should be zero.");
+    }
+    
+    #[test]
+    #[should_panic(expected = "Vectors must be of the same length")]
+    fn test_squared_distance_panics_on_mismatched_lengths() {
+        let vec1 = Vector::new(vec![1.0, 2.0]);
+        let vec2 = Vector::new(vec![1.0, 2.0, 3.0]);
+
+        vec1.squared_distance(&vec2);
+    }
+
+    #[test]
+    fn test_squared_distance_with_empty_vectors() {
+        let vec1 = Vector::new(vec![]);
+        let vec2 = Vector::new(vec![]);
+        
+        let expected_distance = 0.0;
+        assert_eq!(vec1.squared_distance(&vec2), expected_distance, "The distance between two empty vectors should be zero.");
+    }
+}
