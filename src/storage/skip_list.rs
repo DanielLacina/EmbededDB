7318742d7a48e96bb83208e58451d35bd36dd97e
Daniel Lacina <daniellacina8@gmail.com>
2025-09-02 13:47:57 -0500

    working

--- src/storage/skip_list.rs
+++ src/storage/skip_list.rs
@@ -8,7 +8,7 @@ struct Element {
 }
 
 #[derive(Clone)]
-struct Entry {
+pub struct Entry {
     key: usize,
     value: Vec<u8>,
     tombstone: bool,
@@ -20,7 +20,7 @@ impl Entry {
     }
 }
 
-struct SkipList {
+pub struct SkipList {
    max_level: usize,    
    p: f64,
    level: usize,
@@ -29,7 +29,7 @@ struct SkipList {
 }
 
 impl SkipList {
-    fn new(max_level: usize, p: f64) -> Self {
+    pub fn new(max_level: usize, p: f64) -> Self {
         SkipList {
             max_level,
             p,
@@ -39,26 +39,63 @@ impl SkipList {
         }
     }
 
-    fn search(&self, key: &usize) -> Option<Entry> {
+    fn random_level(&self) -> usize {
+        let mut level = 0;
+        while level < self.max_level {
+            if rand::random::<f64>() < self.p {
+                level += 1;
+            } else {
+                break;
+            }
+        }
+        level
+    }
+
+    pub fn get(&self, key: &usize) -> Option<Entry> {
+        self.search(key).map(|element_ref| element_ref.borrow().entry.clone())
+    }
+
+    pub fn delete(&self, key: &usize) -> bool {
+        if let Some(element_ref) = self.search(key) {
+            element_ref.borrow_mut().entry.tombstone = true;
+            true
+        } else {
+            false
+        }
+    }
+
+    // pub fn insert_or_update(&mut self, key: &usize, value: Vec<u8>)  {
+    //     if let Some(element_ref) = self.search(key) {
+    //         element_ref.borrow_mut().entry.tombstone = false;
+    //         element_ref.borrow_mut().entry.value = value;
+    //     } else {
+    //         let level = self.random_level();
+    //         if level > self.
+    //     }
+    // }
+        
+
+    fn search(&self, key: &usize) -> Option<ElementRef> {
         if self.head.is_none() {
             return None;
         }
         let mut current = self.head.clone().unwrap();
-        let mut lc = self.level;
-        while lc > 0 {
-            let next = current.borrow().next.get(lc).cloned();
-            if let Some(node_ref) = next.clone() {
-               let node = node_ref.borrow();
-               if node.entry.key == *key {
-                 return Some(node.entry.clone());
-               } else if node.entry.key > *key {
+        let mut lc = self.level as i64;
+        while lc >= 0 {
+            let next = current.borrow().next.get(lc as usize).cloned();
+            if let Some(next_ref) = next.clone() {
+               let element = next_ref.borrow();
+               if element.entry.key > *key {
                  lc -= 1;
                } else {
-                 current = node_ref.clone(); 
+                 current = next_ref.clone(); 
                }
             } else {
                 lc -= 1;    
             }
+        }
+        if current.borrow().entry.key == *key && !current.borrow().entry.tombstone {
+            return Some(current);
         }
         None
     }
